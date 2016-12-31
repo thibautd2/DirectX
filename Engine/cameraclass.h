4f8aa639a8772a627e4c27d82b4b33e84756935a
thibautd2 <thibaut.betbeder@epita.fr>
2016-12-31 13:11:15 +0100

    Filiage with input error and roation in foliagecless error

---
+++ Engine/cameraclass.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: cameraclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _CAMERACLASS_H_
+#define _CAMERACLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include "DirectXTK\SimpleMath.h"
+using namespace std;
+using namespace DirectX::SimpleMath;
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: CameraClass
+////////////////////////////////////////////////////////////////////////////////
+class CameraClass
+{
+public:
+	CameraClass();
+	CameraClass(const CameraClass&);
+	~CameraClass();
+
+	void SetPosition(float, float, float);
+	void SetRotation(float, float, float);
+
+	Vector3 GetPosition();
+	Vector3 GetRotation();
+
+	void Render();
+	void GetViewMatrix(Matrix&);
+
+	void RenderBaseViewMatrix();
+	void GetBaseViewMatrix(Matrix&);
+
+	void RenderReflection(float);
+	void GetReflectionViewMatrix(Matrix&);
+
+private:
+	float m_positionX, m_positionY, m_positionZ;
+	float m_rotationX, m_rotationY, m_rotationZ;
+	Matrix m_viewMatrix, m_baseViewMatrix, m_reflectionViewMatrix;
+};
+
+#endif
